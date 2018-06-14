221345e78f88070ced3c3f914db599e5828358b9
Shun Nagatsu <shun_september@live.jp>
2018-06-14 19:07:13 +0900

    更新しないでも戻れるようにした

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,29 +4,39 @@
 <div class="container">
   <div id="tab" v-cloak>
     <div class="m-tab-bar">
-        <div class="m-tab" v-for="(tab, index) in tabnavs" v-bind:class="{ 'm-tab-active': tab.isActive }" @click="changeTab(index)">
-          <i v-bind:class="[ tab.cls ]"></i>
-        </div>
+      <div class="m-tab" v-for="(tab, index) in tabnavs" v-bind:class="{ 'm-tab-active': tab.isActive }" @click="changeTab(index)">
+        <i v-bind:class="[ tab.cls ]"></i>
+      </div>
     </div>
-    <div v-if="contents == 0" class="m-tab-contents">
-      TL
-    </div>
-    <div v-if="contents == 1" class="m-tab-contents">
-      検索
-    </div>
-    <div v-if="contents == 2" class="m-tab-contents">
-      @include('photo/upload')
-    </div>
-    <div v-if="contents == 3" class="m-tab-contents">
-      いいね!
-    </div>
-    <div v-if="contents == 4" class="m-tab-contents">
-      @include('user/profile-block')
-    </div>
+    <transition name="fade">
+      <div v-if="contents == 0" class="m-tab-contents">
+        TL
+      </div>
+    </transition>
+    <transition name="fade">
+      <div v-if="contents == 1" class="m-tab-contents">
+        検索
+      </div>
+    </transition>
+    <transition name="fade">
+      <div v-if="contents == 2" class="m-tab-contents">
+        @include('photo/upload')
+      </div>
+    </transition>
+    <transition name="fade">
+      <div v-if="contents == 3" class="m-tab-contents">
+        いいね!
+      </div>
+    </transition>
+    <transition name="fade">
+      <div v-if="contents == 4" class="m-tab-contents">
+        @include('user/profile-block')
+      </div>
+    </transition>
   </div>
 </div>
 <script>
-var tab = new Vue({
+let tab = new Vue({
   el: '#tab',
   data: {
     scrollY: 0,
@@ -40,21 +50,25 @@ var tab = new Vue({
     contents: {{ $tab }}
   },
   methods: {
-    changeTab: function (index) {
-      this.contents = index;
-      for (var i = 0; i < 5; i++) {
+    setTab: function (index) {
+      for (let i = 0; i < 5; i++) {
         this.tabnavs[i].isActive = false;
       }
       this.tabnavs[index].isActive = true;
+    },
+    changeTab: function (index) {
+      this.contents = index;
+      this.setTab(index);
       history.pushState(index,'','/home/'+this.tabnavs[index].name);
     }
   },
   created: function () {
-    var index = this.contents;
-    this.tabnavs[index].isActive = true;
+    let self = this;
+    this.tabnavs[self.contents].isActive = true;
 
     window.onpopstate = function(e) {
-      location.reload();
+      self.contents = e.state;
+      self.setTab(e.state);
     }
   }
 });
